62e2a95e2e32a3e35c638bdd3768086d20589850
nitrohe <nitrohe@163.com>
2019-01-15 23:23:06 +0800

    modify webpack.conf

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,16 +7,16 @@ const CleanPlugin = require('clean-webpack-plugin');
 
 const ROOT_PATH = pathLib.resolve(__dirname);
 const ENTRY_PATH = pathLib.resolve(ROOT_PATH, 'src');
+const ENTRY_PATH_PAGES = pathLib.resolve(ROOT_PATH, 'src/pages');
 const OUTPUT_PATH = pathLib.resolve(ROOT_PATH, 'build');
-console.log(pathLib.resolve(ENTRY_PATH, 'index.js'));
 
-const WebpackBundleSizeAnalyzerPlugin = require('webpack-bundle-size-analyzer').WebpackBundleSizeAnalyzerPlugin;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: {
-        index: ['babel-polyfill',pathLib.resolve(ENTRY_PATH, 'index.js')],
-        vendor: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux', 'redux-saga']
+        index: ['babel-polyfill',pathLib.resolve(ENTRY_PATH_PAGES, './index/index.js')],
+        admin: ['babel-polyfill',pathLib.resolve(ENTRY_PATH_PAGES, './admin/index.js')],
+        vendor: ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux', 'redux-saga', 'react-hot-loader/patch', ]
     },
     output: {
         path: OUTPUT_PATH,
@@ -27,7 +27,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
@@ -82,8 +82,8 @@ module.exports = {
         ]
     },
     plugins: [
-        //new WebpackBundleSizeAnalyzerPlugin('./plain-report.txt'),
-        new BundleAnalyzerPlugin(),
+        //打包分析工具
+        //new BundleAnalyzerPlugin(),
         new CleanPlugin(['build']),
         new ProgressBarPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),//改善chunk传输
@@ -91,11 +91,14 @@ module.exports = {
         //    "progress.env.NODE_ENV": JSON.stringify('production')
         //}),
         new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
+           "progress.env.NODE_ENV": JSON.stringify('production')
         }),
-        //new webpack.optimize.UglifyJsPlugin(), //最小化一切
+        // new webpack.DefinePlugin({
+        //     "process.env": {
+        //         "NODE_ENV": JSON.stringify("production")
+        //     }
+        // }),
+        //new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: false,
             show_copyright: false,
@@ -105,11 +108,43 @@ module.exports = {
                drop_debugger: true,
                drop_console: true
            }
-        }), //最小化一切
-        new HtmlWebpackPlugin({
+        }),
+        /*new HtmlWebpackPlugin({
             title: "Nitrohe's Blog",
             showErrors: true,
-        }),
+        }),*/
+        new HtmlWebpackPlugin({
+			title: "Nitrohe's Blog",
+			meta:{
+				keywords:  "",
+				description: "Nitrohe's Blog"
+			},
+			//chunks:[`${item}/${item}`], //引入的js
+			//template: "./src/template.html",
+			filename : "index.html" , //html位置
+			minify:{//压缩html
+				collapseWhitespace: true,
+				preserveLineBreaks: true
+			},
+            chunks: ['manifest', 'vendor', 'index'],
+            chunksSortMode:'dependency'
+		}),
+        new HtmlWebpackPlugin({
+			title: "Nitrohe's Admin",
+			meta:{
+				keywords:  "",
+				description: "Nitrohe's Admin"
+			},
+			//chunks:[`${item}/${item}`], //引入的js
+			//template: "./src/template.html",
+			filename : "admin.html" , //html位置
+			minify:{//压缩html
+				collapseWhitespace: true,
+				preserveLineBreaks: true
+			},
+            chunks: ['manifest', 'vendor', 'admin'],
+            chunksSortMode:'dependency'
+		}),
         new webpack.NoEmitOnErrorsPlugin(),//保证出错时页面不阻塞，且会在编译结束后报错
         new ExtractTextPlugin({
             filename:'bundle.[contenthash].css',
@@ -128,6 +163,12 @@ module.exports = {
         })
     ],
     resolve: {
+        alias:{
+			'@components':pathLib.resolve(__dirname,'./src/components'),
+			'@reducers':pathLib.resolve(ROOT_PATH, './src/reducers'),
+            //'@reducers':'./src/reducers',
+            '@mlib':pathLib.resolve(__dirname,'./mlib')
+		},
         extensions: ['.js', '.json', '.sass', '.scss', '.less', 'jsx']
     }
 };
